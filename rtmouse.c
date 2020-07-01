6c3d396c9683b8cb9288bb6a0ced555b30c72dd1
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-07-01 12:09:23 +0900

    chore: Declare License

--- rtmouse.c
+++ rtmouse.c
@@ -1,3 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *
+ * rtmouse.c
+ * Jetson Nano Mouse device driver
+ *
+ * Version: 0.1.2
+ *
+ * Copyright (C) 2019-2020 RT Corporation <shop@rt-net.jp>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
 #include <linux/cdev.h>
 #include <linux/delay.h>
 #include <linux/device.h>
