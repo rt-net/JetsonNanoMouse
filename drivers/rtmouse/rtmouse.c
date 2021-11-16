dc9b50515a0010a6dbc4c03b9e3d17d613f03536
Daisuke Sato <daisuke.sato@rt-net.jp>
2021-11-16 19:51:35 +0900

    Apply clang-format

--- drivers/rtmouse/rtmouse.c
+++ drivers/rtmouse/rtmouse.c
@@ -11,12 +11,12 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -42,17 +42,17 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1.3");
 
 /* --- GPIO Pins --- */
-#define GPIO_LED0 13	 // PIN22
-#define GPIO_LED1 15	 // PIN18
-#define GPIO_LED2 232	// PIN16
-#define GPIO_LED3 79	 // PIN16
-#define GPIO_SW0 78	  // PIN40
-#define GPIO_SW1 12	  // PIN37
-#define GPIO_SW2 77	  // PIN38
-#define GPIO_SEN_R 194       // PIN15
-#define GPIO_SEN_L 149       // PIN29
-#define GPIO_SEN_RF 14       // PI13
-#define GPIO_SEN_LF 50       // PIN11
+#define GPIO_LED0 13	     // PIN22
+#define GPIO_LED1 15	     // PIN18
+#define GPIO_LED2 232	     // PIN16
+#define GPIO_LED3 79	     // PIN16
+#define GPIO_SW0 78	     // PIN40
+#define GPIO_SW1 12	     // PIN37
+#define GPIO_SW2 77	     // PIN38
+#define GPIO_SEN_R 194	     // PIN15
+#define GPIO_SEN_L 149	     // PIN29
+#define GPIO_SEN_RF 14	     // PI13
+#define GPIO_SEN_LF 50	     // PIN11
 #define GPIO_MOTOR_EN 200    // PIN31
 #define GPIO_MOTOR_DIR_R 168 // PIN32
 #define GPIO_MOTOR_DIR_L 216 // PIN7
